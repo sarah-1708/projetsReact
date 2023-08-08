414289ce02ab23e764a1da9ea376094500b73c71
Sarah RAMBAUD <sarah.rambaud9@gmail.com>
2023-08-08 15:49:52 +0200

    add countries informations

---
+++ 3.GeoData/src/components/Listcard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useState } from 'react'
+import { createPortal } from 'react-dom'
+import ModalContent from './ModalContent'
+
+export default function Listcard({country}) {
+const[showModal, setShowModal]=useState(false)
+
+  return (
+    <>
+   <li onClick={() => setShowModal(!showModal)} className="relative cursor-pointer rounded transition-transform duration-300 hover:-translate-y-1 will-change-transform">
+    <h2 className="absolute left-0 top-0 p-2 bg-gray-50 drop-show-[2px_2px_1px rgba(0,0,0,0.5)] text-xl rounded">{country.name.common}</h2>
+    <img src={country.flags.svg} className="w-full h-full object-cover rounded" alt="country flag" />
+   </li>
+   {showModal && createPortal(<ModalContent country={country} closeModal={() => setShowModal(!showModal)}/>, document.body)}
+   </>
+  )
+}
