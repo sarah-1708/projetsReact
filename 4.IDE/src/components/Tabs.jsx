9153f414c2d23c88f38bdd1d8aac00c00d3d92d2
Sarah RAMBAUD <sarah.rambaud9@gmail.com>
2023-08-10 11:07:03 +0200

    add IDE Project

---
+++ 4.IDE/src/components/Tabs.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import CodeTab from './CodeTab'
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import ButtonTab from './ButtonTab';
+import Preview from './Preview';
+
+export default function Tabs() {
+
+    const tabs = useSelector(state => state.tabs)
+    console.log(tabs);
+    const previewData = useSelector(state => state.preview)
+    const [tabIndex, setTabIndex]= useState(tabs[0].id)
+
+  return (
+    <div className="flex grow">
+        <div className="grow flex flex-col w-[175px] shrink-0 text-slate-300 border-r border-slate-200">
+            {tabs.map(tab=>(
+                <ButtonTab
+                key={tab.id}
+                id={tab.id}
+                toggleTab={id=>setTabIndex(id)}
+                imgURL={tab.imgURL}
+                buttonContent={tab.buttonContent}
+                />
+                ))}
+        </div>
+        <div className="w-full grow relative">
+            <CodeTab
+            id={tabIndex}
+            code={tabs.find(obj => obj.id === tabIndex).code}
+            />
+            {previewData.preview && <Preview/> }
+            
+        </div>
+      
+    </div>
+  )
+}
